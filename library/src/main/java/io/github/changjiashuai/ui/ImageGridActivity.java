2750d6909f71f9b6283b4f9ff3e8ab524981a2ab
changjiashuai <changjiashuai@gmail.com>
2017-02-22 16:21:50 +0800

    1、init ui 2、add photoView library

---
+++ library/src/main/java/io/github/changjiashuai/ui/ImageGridActivity.java
@@ -0,0 +1,15 @@
+package io.github.changjiashuai.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.github.changjiashuai.library.R;
+
+public class ImageGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_grid);
+    }
+}
