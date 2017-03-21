2fba4a054d8875603e7a14e9cdacda1b2142fd34
yang.guo <gy@fir.im>
2017-03-21 17:13:50 +0800

    test

--- app/src/test/java/io/github/changjiashuai/imagepicker/ExampleUnitTest.java
+++ app/src/test/java/io/github/changjiashuai/imagepicker/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package io.github.changjiashuai.imagepicker;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,6 +14,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
+        System.out.println("====================================");
         assertEquals(4, 2 + 2);
     }
 }
